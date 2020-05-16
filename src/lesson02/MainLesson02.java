4413385d6e796f6a58f7eae729f7106cf2e1dcf9
Vladimir Svishch <5693031@gmail.com>
2020-05-16 13:44:47 +0300

    hw2

---
+++ src/lesson02/MainLesson02.java
@@ -0,0 +1,15 @@
+package lesson02;
+public class MainLesson02 {
+
+/*
+    ФИО:  		    Свищ Владимир Сергеевич
+    Факультет:      Geek University Android-разработки
+    Курс: 		    Java Core. Базовый уровень
+    Урок: 		    Урок 2. Основные конструкции
+    Дата правки:    16.05.2020
+*/
+
+    public static void main(String[] args) {
+
+    }
+}
