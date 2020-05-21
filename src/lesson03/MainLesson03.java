31a0cb5404aab6e086af507c729231db40a356bf
Vladimir Svishch <5693031@gmail.com>
2020-05-21 19:48:00 +0300

    hw3 - creature

---
+++ src/lesson03/MainLesson03.java
@@ -0,0 +1,15 @@
+package lesson03;
+
+/*
+    ФИО:  		    Свищ Владимир Сергеевич
+    Факультет:      Geek University Android-разработки
+    Курс: 		    Java Core. Базовый уровень
+    Урок: 		    Урок 3. Практика
+    Дата правки:    21.05.2020
+*/
+
+public class MainLesson03 {
+    public static void main(String[] args) {
+
+    }
+}
