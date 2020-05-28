5f23622603f365290a85148b620dd89f8b9425c9
Vladimir Svishch <5693031@gmail.com>
2020-05-28 17:36:02 +0300

    hw5

---
+++ src/lesson05/MainLesson05.java
@@ -0,0 +1,15 @@
+package lesson05;
+
+/*
+    ФИО:  		    Свищ Владимир Сергеевич
+    Факультет:      Geek University Android-разработки (478 - 12.05.2020)
+    Курс: 		    Java Core. Базовый уровень
+    Урок: 		    Урок 5. Введение в ООП
+    Дата правки:    28.05.2020
+*/
+
+public class MainLesson05 {
+    public static void main(String[] args) {
+
+    }
+}
